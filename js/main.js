dcdc4bad02fa394268a55f6ae7492c198b1163e0
Богдан Котов <bbrudastyy@gmail.com>
2020-09-26 22:00:30 +0300

    Исправил ошибки линта

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const ROOM_TYPE = ['palace', 'flat', 'house', 'bungalow'];
 const ROOM_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
@@ -14,7 +14,7 @@ let fragments = [];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
-};
+}
 
 function getRandomArr(constArr) {
   const length = getRandomInt(constArr.length);
@@ -26,9 +26,9 @@ function getRandomArr(constArr) {
     helpArr.splice(index, 1);
   }
   return newArr;
-};
+}
 
-function fillRoomsInfo () {
+function fillRoomsInfo() {
   for (let i = 1; i < 9; i++) {
     let room = {
       author: {
@@ -36,27 +36,27 @@ function fillRoomsInfo () {
       },
       offer: {
         title: `Title${i}`,
-          address: `${getRandomInt(600)}, ${getRandomInt(350)}`,
-          price: getRandomInt(10000),
-          type: ROOM_TYPE[i % 4],
-          rooms: getRandomInt(4),
-          guests: getRandomInt(3),
-          checkin: `${12 + getRandomInt(2)}+ :00`,
-          checkout: `${12 + getRandomInt(2)}+ :00`,
-          features: getRandomArr(ROOM_FEATURES),
-          description: `description${i}`,
-          photos: getRandomArr(ROOM_PHOTOS)
-        },
+        address: `${getRandomInt(600)}, ${getRandomInt(350)}`,
+        price: getRandomInt(10000),
+        type: ROOM_TYPE[i % 4],
+        rooms: getRandomInt(4),
+        guests: getRandomInt(3),
+        checkin: `${12 + getRandomInt(2)}+ :00`,
+        checkout: `${12 + getRandomInt(2)}+ :00`,
+        features: getRandomArr(ROOM_FEATURES),
+        description: `description${i}`,
+        photos: getRandomArr(ROOM_PHOTOS)
+      },
       location: {
         x: getRandomInt(MAP.offsetWidth - 40),
         y: 130 + getRandomInt(500)
       }
-    }
+    };
     rooms.push(room);
   }
-};
+}
 
-function fillTemplate () {
+function fillTemplate() {
   for (let i = 0; i < rooms.length; i++) {
     let fragment = PIN_TEMPLATE.cloneNode(true);
     fragment.setAttribute(`style`, `left: ${rooms[i].location.x + PIN_WIDTH / 2}px; top: ${rooms[i].location.y + PIN_HEIGHT / 2}px`);
